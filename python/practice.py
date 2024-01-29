187d4f81eedbdc29d89a2eefa86d0a678264e10e
MahdiAlsalami <mahdialsalami0@gmail.com>
2024-01-28 20:36:52 -0800

    Added question using input

--- python/practice.py
+++ python/practice.py
@@ -3,3 +3,8 @@ num_two = 22
 num_three = 22
 
 print("Jacob is " + str(num) + " years old.")
+
+
+name = input("What is your name?: ")
+age = input("What is your age?: ")
+print(f"Your name is {name} and you are {age} years old")
