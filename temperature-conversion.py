76b62807e3fff93a548aa9d797a29fefeb692186
MahdiAlsalami <mahdialsalami0@gmail.com>
2024-01-28 22:27:37 -0800

    Changed line10 to elif and math was wrong (correct now)

--- temperature-conversion.py
+++ temperature-conversion.py
@@ -6,9 +6,9 @@ temp = float(input("What is the unit you currenly have?: "))
 
 if unit == "C":
   math = (temp * 1.8) + 32
-  print(f"Your temperature in Fahrenheit is: {math}")
-if unit == "F":
-  math = (temp - 32) * 1.8
-  print(f"Your temperature in Celsius is: {math}")
+  print(f"Your temperature in Fahrenheit is: {round(math, 4)}")
+elif unit == "F":
+  math = (temp - 32) * 5/9
+  print(f"Your temperature in Celsius is: {round(math, 4)}")
 else:
   print("Somethings wrong try again...")
