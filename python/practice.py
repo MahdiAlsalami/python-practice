59b1990c07a1057f2a3e69d0dc6209a5b2724a69
MahdiAlsalami <mahdialsalami0@gmail.com>
2024-02-13 15:27:11 -0800

    used logical operators

--- python/practice.py
+++ python/practice.py
@@ -1,7 +1,24 @@
-print('Weight Converter(Pounds -> Kilograms)')
+'''print('Weight Converter(Pounds -> Kilograms)')
 
 weight = int(input("What is your weight in pounds: "))
 
 kilo = weight * 0.453592
 
-print(f"Your weight in kilograms is {kilo}")
+print(f"Your weight in kilograms is {kilo}")'''
+
+
+high_income = True
+good_credit = True
+
+if high_income and good_credit:
+  print("Eligible for loan")
+elif high_income == True and good_credit ==False:
+  print("Not eligible for loan")
+elif high_income == False and good_credit == True:
+  print("Not eligible for loan")
+elif high_income == False and good_credit == False:
+  print("Not eligible for loan")
+else:
+  print("Have a good day!")
+
+
