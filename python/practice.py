63d470cd756f8c7e951b680b1764e5b1e66b831e
MahdiAlsalami <mahdialsalami0@gmail.com>
2024-02-12 17:20:28 -0800

    Created simple weight conversion

--- python/practice.py
+++ python/practice.py
@@ -1,51 +1,7 @@
-'''import math
+print('Weight Converter(Pounds -> Kilograms)')
 
-print("Calculate the Circumfirance of a Circle!")
-r = float(input("Input the radius: "))
-c = 2 * math.pi * r
+weight = int(input("What is your weight in pounds: "))
 
-print(c)
+kilo = weight * 0.453592
 
-import math
-
-a = float(input("Enter angle side A: "))
-b = float(input("Enter angle side B: "))
-
-area = math.sqrt(math.pow(a,2)+ math.pow(b,2))
-
-print(f"Side C is : {area}")'''
-
-'''cake = input("What is your favorite cake out of (Chocolate, Red Velvet, or Vanilla)?: ")
-
-if cake == "Chocolate":
-  print("Your on the DARK side :/")
-elif cake == "Red Velvet":
-  print("Your on the RED side :/")
-elif cake == "Vanilla":
-  print("Your on the WHITE side :/")
-else:
-  print("Somethings WRONG try again...")'''
-
-
-
-
-
-#num = 22
-#num_two = 22
-#num_three = 22
-
-#print("Jacob is " + str(num) + " years old.")
-
-
-#name = input("What is your name?: ")
-#age = input("What is your age?: ")
-#print(f"Your name is {name} and you are {age} years old!")
-
-#x = 3.14
-#y = 4
-#z = 5
-
-#result = round(x)
-#result = abs(y)
-#result = pow(y,2)
-#print(result)
+print(f"Your weight in kilograms is {kilo}")
